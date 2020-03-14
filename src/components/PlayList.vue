9a1b0d3b724318ace9651696e1069cdcd0a220bb
superBoyJack <49631199+superBoyJack@users.noreply.github.com>
2020-03-15 01:05:38 +0800

    更正下载文件的文件名

--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -269,8 +269,55 @@ export default {
             console.log(error);
         });
     },
-    // downloadFile(url,name,type){
-    // },
+    
+    /*
+     * 下载
+     * @param  {String} url 目标文件地址
+     * @param  {String} filename 想要保存的文件名称
+     */
+    courseDownload(url, filename) {
+        var _this = this;
+        _this.getBlob(url, function(blob) {
+            _this.saveAs(blob, filename);
+        })
+    },
+    getBlob(url,cb) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                cb(xhr.response);
+            }
+        };
+        xhr.send();
+    },
+    /**
+     * 保存
+     * @param  {Blob} blob
+     * @param  {String} filename 想要保存的文件名称
+     */
+    saveAs(blob, filename) {
+        if (window.navigator.msSaveOrOpenBlob) {
+            navigator.msSaveBlob(blob, filename);
+        } else {
+            var link = document.createElement('a');
+            var body = document.querySelector('body');
+     
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+     
+            // fix Firefox
+            link.style.display = 'none';
+            body.appendChild(link);
+     
+            link.click();
+            body.removeChild(link);
+     
+            window.URL.revokeObjectURL(link.href);
+        }
+        },
+    
     // 根据url下载文件
         downloadFile (url,name,type) {
             
@@ -284,6 +331,7 @@ export default {
             link.style.display = 'none'
             link.href = url
             link.setAttribute('download', name + "." + type)
+            link.setAttribute('onclick', this.courseDownload(url, name + "." + type));
             document.body.appendChild(link)
             link.click()
         },
